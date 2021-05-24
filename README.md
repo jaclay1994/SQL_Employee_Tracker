c0073122e7c64bfd1d441a691d4f9fd93e99a8f3
jaclay1994 <jaclay94@gmail.com>
2021-05-23 23:53:10 -0400

    Updated functionality to view user inputs.

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
-# SQL_Employee_Tracker
+## SQL_Employee_Tracker
+
+## Description
+
+The employee tracker application will allow users to create a database for roles departments, managers, and employees. This will app is simple and easy to use, helping anyone needing to create departmental structure.
+
+## Progress
+
+This application has the ability to create and view departments, roles, employees and roles. Further updates will be added to allow the user to update and remove information from the database as well. This is due to limited knowledge on SQL syntax. However, the core functionality of creating a database is functional.
+
+## Video Link
+https://youtu.be/V6qtLH-0RnU
+
+## License
+
+Copyright (c) [2021] [Julian Clay]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
--- schema.js
+++ schema.js
@@ -19,7 +19,7 @@ const start = () => {
                 type: "list",
                 name: "start_prompt",
                 message: "Welcome to the employee database. Pick an option.",
-                choices: ["Add Department", "Add Employee", "Add Manager", "Add Role"],
+                choices: ["Add Department", "Add Employee", "Add Manager", "Add Role", "View Employees", "View Roles", "View Departments", "View Managers"]
             }
             // {
             //     type: "list",
@@ -36,176 +36,217 @@ const start = () => {
                 managerAdd();
             } else if (answer.start_prompt === "Add Role") {
                 roleAdd();
+            } else if (answer.start_prompt === "View Employees") {
+                employeeView();
+            } else if (answer.start_prompt === "View Roles") {
+                roleView();
+            } else if (answer.start_prompt === "View Departments") {
+                departmentView();
+            } else if (answer.start_prompt === "View Managers") {
+                managerView();
             } else {
                 connection.end();
             }
 
         });
-};
-start()
-
-const departmentAdd = () => {
-    inquirer
-        .prompt([
-            {
-                name: "departmentName",
-                type: "input",
-                message: "What is the name of the department?"
-            },
-        ])
-        .then((answer) => {
-            connection.query(
-                "INSERT INTO department_info SET ?",
-                {
-                    department_name: answer.departmentName
-                },
-                (err) => {
-                    if (err) throw err;
-                    console.log("Department name added!");
-                    start();
-                })
-        });
-}
-
-const roleAdd = () => {
-    connection.query('SELECT * FROM department_info', (err, results) => {
-    inquirer
-        .prompt([
-            {
-                name: "role",
-                type: "input",
-                message: "What is the name of the role?"
-            },
-            {
-                name: "salary",
-                type: "input",
-                message: "What is the salary for this role?"
-            },
-            {
-                name: "department",
-                type: "list",
-                choices() {
-                    const departmentArray = [];
-                    results.forEach(({ id, department_name }) => {
-                        departmentArray.push(id + " " + department_name);
-                    });
-                    return departmentArray
-                },
-                message: "What department does this role belong to?"
-            },
-
-        ])
-        .then((answer) => {
-            connection.query(
-                "INSERT INTO role_id SET ?",
-                {
-                    title: answer.role,
-                    salary: answer.salary,
-                    department_id: answer.department[0]
-                },
-                (err) => {
-                    if (err) throw err;
-                    console.log("Role added!");
-                    start();
-                })
-        });
-    })
-}
-
-const managerAdd = () => {
-    connection.query('SELECT * FROM manager', (err, results) => {
-    inquirer
-        .prompt([
-            {
-                name: "managerFirst",
-                type: "input",
-                message: "What is the first name of the manager?"
-            },
-            {
-                name: "managerLast",
-                type: "input",
-                message: "What is the the last name?"
-            }
-
-        ])
-        .then((answer) => {
-            connection.query(
-                "INSERT INTO manager SET ?",
-                {
-                    first_name: answer.managerFirst,
-                    last_name: answer.managerLast
         
-                },
-                (err) => {
-                    if (err) throw err;
-                    console.log("Manager added!");
-                    start();
-                })
-        });
-    })
-}
-
-const employeeAdd = () => {
-    connection.query('SELECT * FROM role_id', (err, results) => {
-        inquirer
+        
+        const departmentAdd = () => {
+            inquirer
             .prompt([
                 {
-                    name: "firstName",
+                    name: "departmentName",
                     type: "input",
-                    message: "What is the employee's first name?"
-                },
-                {
-                    name: "lastName",
-                    type: "input",
-                    message: "What is the employee's last name?"
-                },
-                {
-                    name: "role",
-                    type: "list",
-                    choices() {
-                        const roleArray = [];
-                        results.forEach(({ title }) => {
-                            roleArray.push(title);
-                        });
-                        return roleArray
-                    },
-                    message: "Select a role."
-                },
-                {
-                    name: "managerChoice",
-                    type: "list",
-                    choices() {
-                        const managerArray = [];
-                        results.forEach(({ id, first_name, last_name }) => {
-                            connection.query('SELECT * FROM manager', (err, results) => {
-                            managerArray.push(id + " " + first_name + " " + last_name);
-                            })
-                        });
-                        return managerArray
-                    },
-                    message: "Choose a manager."
+                    message: "What is the name of the department?"
                 },
             ])
             .then((answer) => {
                 connection.query(
-                    "INSERT INTO employee_id SET ?",
+                    "INSERT INTO department_info SET ?",
                     {
-                        first_name: answer.firstName,
-                        last_name: answer.lastName,
-                        role_id: answer.role[1],
-                        manager_id: answer.managerChoice[0]
-
+                        department_name: answer.departmentName
                     },
                     (err) => {
                         if (err) throw err;
-                        console.log("Employee added!");
+                        console.log("Department name added!");
                         start();
                     })
-            });
-    })
-}
-
-// function databaseFunction() {
+                });
+            }
+            
+            const roleAdd = () => {
+                connection.query('SELECT * FROM department_info', (err, results) => {
+                    inquirer
+                    .prompt([
+                        {
+                            name: "role",
+                            type: "input",
+                            message: "What is the name of the role?"
+                        },
+                        {
+                            name: "salary",
+                            type: "input",
+                            message: "What is the salary for this role?"
+                        },
+                        {
+                            name: "department",
+                            type: "list",
+                            choices() {
+                                const departmentArray = [];
+                                results.forEach(({ id, department_name }) => {
+                                    departmentArray.push(id + " " + department_name);
+                                });
+                                return departmentArray
+                            },
+                            message: "What department does this role belong to?"
+                        },
+                        
+                    ])
+                    .then((answer) => {
+                        connection.query(
+                            "INSERT INTO role_id SET ?",
+                            {
+                                title: answer.role,
+                                salary: answer.salary,
+                                department_id: answer.department[0]
+                            },
+                            (err) => {
+                                if (err) throw err;
+                                console.log("Role added!");
+                                start();
+                            })
+                        });
+                    })
+                }
+                
+                const managerAdd = () => {
+                    connection.query('SELECT * FROM manager', (err, results) => {
+                        inquirer
+                        .prompt([
+                            {
+                                name: "managerFirst",
+                                type: "input",
+                                message: "What is the first name of the manager?"
+                            },
+                            {
+                                name: "managerLast",
+                                type: "input",
+                                message: "What is the the last name?"
+                            }
+                            
+                        ])
+                        .then((answer) => {
+                            connection.query(
+                                "INSERT INTO manager SET ?",
+                                {
+                                    first_name: answer.managerFirst,
+                                    last_name: answer.managerLast
+                                    
+                                },
+                                (err) => {
+                                    if (err) throw err;
+                                    console.log("Manager added!");
+                                    start();
+                                })
+                            });
+                        })
+                    }
+                    
+                    const employeeAdd = () => {
+                        connection.query('SELECT * FROM role_id', (err, results) => {
+                            inquirer
+                            .prompt([
+                                {
+                                    name: "firstName",
+                                    type: "input",
+                                    message: "What is the employee's first name?"
+                                },
+                                {
+                                    name: "lastName",
+                                    type: "input",
+                                    message: "What is the employee's last name?"
+                                },
+                                {
+                                    name: "role",
+                                    type: "list",
+                                    choices() {
+                                        const roleArray = [];
+                                        results.forEach(({ title }) => {
+                                            roleArray.push(title);
+                                        });
+                                        return roleArray
+                                    },
+                                    message: "Select a role."
+                                }
+                                // {
+                                    //     name: "managerChoice",
+                                    //     type: "list",
+                                    //     choices() {
+                                        //         const managerArray = [];
+                                        //         connection.query('SELECT * FROM manager', (err, results) => {
+                                            //         results.forEach(({ id, first_name, last_name }) => {
+                                                //             managerArray.push(id + " " + first_name + " " + last_name);
+                                                //             })
+                                                //         });
+                                                //         return managerArray
+                                                //     },
+                                                //     message: "Choose a manager."
+                                                // },
+                                            ])
+                                            .then((answer) => {
+                                                connection.query(
+                                                    "INSERT INTO employee_id SET ?",
+                                                    {
+                                                        first_name: answer.firstName,
+                                                        last_name: answer.lastName,
+                                                        role_id: answer.role_id,
+                                                        // manager_id: answer.managerChoice[0]
+                                                        
+                                                    },
+                                                    (err) => {
+                                                        if (err) throw err;
+                                                        console.log("Employee added!");
+                                                        start();
+                                                    })
+                                                });
+                                            })
+                                        }
+                                        
+                                        const employeeView = () => {
+                                            connection.query('SELECT * FROM employee_id', (err, results) => {
+                                                console.log(results);
+                                                start();
+                                                if (err) throw err;
+                                            })
+                                        }
+                                        
+                                        const managerView = () => {
+                                            connection.query('SELECT * FROM manager', (err, results) => {
+                                                console.log(results);
+                                                start();
+                                                if (err) throw err;
+                                            })
+                                        }
+                                        
+                                        const roleView = () => {
+                                            connection.query('SELECT * FROM role_id', (err, results) => {
+                                                console.log(results);
+                                                start();
+                                                if (err) throw err;
+                                            })
+                                        }
+                                        
+                                        const departmentView = () => {
+                                            connection.query('SELECT * FROM department_info', (err, results) => {
+                                                console.log(results);
+                                                start();
+                                                if (err) throw err;
+                                            })
+                                        }
+                                        
+                                    };
+                                    start()
+                                        // function databaseFunction() {
 //     connection.query('SELECT * FROM department_info', (err, res) => {
 //         if (err) throw err;
 //         res.forEach(({ id, department_name, title, salary, department_id, first_name, last_name, role_id, manager_id }) => {
